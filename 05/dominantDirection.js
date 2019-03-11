9336f2a488d079564fa298e8640d3af1d89854a0
Thanwa Puamanee <zthanwa.p@pttict.com>
2019-03-11 23:21:40 +0700

    add dominant direction

---
+++ 05/dominantDirection.js
@@ -0,0 +1,23 @@
+function dominantDirection(text) {
+    var chars = [];
+    for (let char of text) {
+        chars.push(characterScript(char.codePointAt(0)));
+
+    }
+    return countBy(chars, (x) => { return x ? x.direction : "" })
+        .reduce((res, cur) => {
+
+            if (cur.count > res.count)
+                return cur;
+
+            return res
+
+        }).name
+}
+
+
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+  // → rtl
